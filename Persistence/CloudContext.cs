41a20385c9d55b54a7abc4169c183bb1cff28bce
Diego Vergara <diego.github@gmail.com>
2018-05-01 22:48:01 -0300

    repository arch #1

---
+++ Persistence/CloudContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Persistence
+{
+    public class CloudContext : DbContext
+    {
+        public CloudContext(DbContextOptions<CloudContext> options)
+           : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
---
+++ Persistence/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Persistence
+{
+    public interface IRepository<T> where T : class
+    {
+        T Get(int id);
+        IEnumerable<T> GetAll();
+        IEnumerable<T> GetList(int pageIndex, int pageSize);
+        IEnumerable<T> Find(Expression<Func<T, bool>> predicate);
+       
+        void Add(T entity);
+        void AddRange(IEnumerable<T> entities);
+
+        void Delete(T entity);
+        void DeleteRange(IEnumerable<T> entities);
+
+        void Edit(T entity);
+    }
+}
